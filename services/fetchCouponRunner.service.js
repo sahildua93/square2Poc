ec4a7cf6357b1d9fc5c72fe0e8c97c8d55874036
sahildua93 <sahil.dua@tothenew.com>
2018-04-09 18:37:06 +0530

    coupon retrieved successfully for show code and instant btns

--- services/fetchCouponRunner.service.js
+++ services/fetchCouponRunner.service.js
@@ -3,40 +3,39 @@ const childProcess = require('child_process');
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
 
-// const Iterate_Containers = (count, total, url) => {
-//     // const split1 = url.split('https://www.vipon.com/product/');
-//     // const split2 = split1[1].split('-', 1);
-//     if (count < total - 1) {
-//         console.log(url);
+// const Iterate_Containers = url => {
+//     const split1 = url.split('https://www.vipon.com/product/');
+//     const split2 = split1[1].split('-', 1);
+//
+//     // if (count < total - 1) {
 //         nightmare
-//             .click('a[href="/product/4930058-Baby-Shower-Advice-Prediction-Cards-Keepsake-Game-for?ref=ss_4930058_18_image"]');
-//         // .then((gotoResult) => {
-//         //     nightmare.wait('.st-animated');
-//         //     nightmare.evaluate(() => {
-//         //         const text = document.querySelector('.request-btn').textContent;
-//         //         return text;
-//         //     })
-//         //     .then((data) => {
-//         //         console.log('data--->', data);
-//         //     });
-//         // });
-//         return Iterate_Containers(count + 1);
-//     }
+//             .goto(`${url}`)
+//             .wait('.st-animated')
+//             .evaluate(() => {
+//                 const text = document.querySelector('.request-btn').textContent;
+//                 return text;
+//             })
+//             .then(result => {
+//                 results.push(result);
+//             });
+//         console.log('results0--->>', results);
+//         // return Iterate_Containers(count + 1);
+//     // }
 // };
 
 const couponService = (runConfig, cb) => {
-     nightmare
+    nightmare
         .goto('https://www.vipon.com/login')
         .type('#loginform-email', 'sahil.dua@tothenew.com')
         .type('#loginform-password', 'fernandotorres')
         .click('#signup-button')
         .wait('.show_user_email')
-         .then(cookies => {
-             console.log('cookies--->', cookies);
-             // do something with the cookies
-         })
-        // .click('a[href="/promotion/index?type=instant"]')
-        // .wait('.box')
+        // .then(cookies => {
+        //      console.log('cookies--->', cookies);
+        //      // do something with the cookies
+        // })
+        .click('a[href="/promotion/index?type=instant"]')
+        .wait('.box')
 
         // --- scroll to bottom code --------------------
         // let previousHeight, currentHeight = 0;
@@ -49,21 +48,40 @@ const couponService = (runConfig, cb) => {
         //         .wait(3000);
         // }
 
-        // ---- specific pproduct screen for coupon retrieval -----------
-        // .evaluate(() => {
-        //     const dataId = [];
-        //     const count = document.querySelector('#product-box-container').childElementCount;
-        //     for(let i=0; i < count; i++) {
-        //         const url = document.querySelectorAll('div.img-container')[i].firstElementChild.href;
-        //         const requiredUrl = url.split('https://www.vipon.com');
-        //         dataId.push(requiredUrl[1]);
-        //     }
-        //     return dataId;
-        // })
-        // .then(urls => {
-        //     urls.forEach((url, i) => Iterate_Containers(i, urls.length, url));
-        // })
-        .catch(error => {
+        // ---- specific product screen for coupon retrieval -----------
+        .evaluate(() => {
+            const dataId = [];
+            const count = document.querySelector('#product-box-container').childElementCount;
+            for(let i=0; i < count; i++) {
+                const url = document.querySelectorAll('div.img-container')[i].firstElementChild.href;
+                // const requiredUrl = url.split('https://www.vipon.com');
+                dataId.push(url);
+            }
+            return dataId;
+        })
+
+        .then(async urls => {
+            for(const url of urls) {
+                await nightmare
+                    .goto(`${url}`)
+                    .wait('.st-animated')
+                    .evaluate(() =>  document.querySelector('.request-btn').textContent)
+                    .then(async result => {
+                        if (result === 'Instant Deal' || result === 'Show Code')
+                            await nightmare
+                                .click('#product-request-btn')
+                                .wait(() => {
+                                    let text = document.querySelector('#voucherContent').textContent;
+                                    return text.length !== 1;
+                                })
+                                .evaluate(() => document.querySelector('#voucherContent').textContent)
+                                .then(code => {
+                                    console.log('code is --->> ', code);
+                                })
+                    })
+            }
+        });
+        nightmare.catch(error => {
             console.error('Search failed:', error)
         });
 };
